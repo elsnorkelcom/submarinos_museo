24f14353f9c9ea3a01f8f1f2ef0a95fcfb5304e6
Daniel Alberto Mesa <162736977+elsnorkelcom@users.noreply.github.com>
2024-03-09 07:02:09 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,4 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1cedHXsWAhKSfJftKRmG7ZZRpdvJMjS6skTrV_JK2MqM/edit?usp=sharing';
-
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1dzcsuaZXVEA-0Xtq-GjO_9n3rn0fb2bkxmRs7omi0L8/edit?usp=sharing';
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyCSzgGrItibLIaOJg6N0bFrxTCyobpKPDY';
